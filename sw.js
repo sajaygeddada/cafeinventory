95c111899132488f7eee6b083e0cf27e923361b1
Balasajay Geddada <75261619+sajaygeddada@users.noreply.github.com>
2026-08-05 12:57:06 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'sajays-cafe-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
+
+// Install the service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch resources
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
